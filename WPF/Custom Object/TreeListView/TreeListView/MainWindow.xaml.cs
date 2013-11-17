ffbed147ec3396e8183c74ec62d1025103ad5732
xavier <xavierbollart@orange.fr>
2013-11-17 23:36:28 +0100

    add custom treeListView

---
+++ WPF/Custom Object/TreeListView/TreeListView/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeListView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static ObservableCollection<Topic> Topics = new ObservableCollection<Topic>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Topics.Add(new Topic("Desk 1", "544", "10", "3", "4"));
+            Topics.Add(new Topic("Desk 2", "11", "22", "33", "44"));
+            Topic DataGridTopic = new Topic("Desk 3", "0.1", "0.2", "0.3", "0.4");
+            DataGridTopic.ChildTopics.Add(new Topic("Equity","20/22","26/20","75/100","25/50"));
+            DataGridTopic.ChildTopics.Add(new Topic("Forex", "20/22", "26/20", "75/100", "25/50"));
+            DataGridTopic.ChildTopics.Add(new Topic("Options", "20/22", "26/20", "75/100", "25/50"));
+            Topics.Add(DataGridTopic);
+
+                
+            myTreeView.DataContext = Topics;
+        }
+
+        private void myTreeView_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void myTreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+
+        }
+    }
+}
---
+++ WPF/Custom Object/TreeListView/TreeListView/Topic.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace TreeListView
+{
+    public class Topic
+    {
+        private static int ratio = 100;
+
+        private double outstandSell = 25;
+        private double outstandBuy = 75;
+        private double delta = 20;
+        private double notional = 26;
+
+        private double outstandSellLevel = 50;
+        private double outstandBuyLevel = 100;
+        private double deltaLevel = 22;
+        private double notionalLevel = 20;
+
+        public string Name { get; set; }
+        public string Delta { get; set; }
+        public string Notional { get; set; }
+        public string OutstandBuy { get; set; }
+        public string OutstandSell { get; set; }
+
+        public double OutstandSellLimitState { get { return Percentage(outstandSell, outstandSellLevel); } }
+        public double OutstandBuyLimitState { get { return Percentage(outstandBuy, outstandBuyLevel); } }
+        public double DeltaLimitState { get { return Percentage(delta, deltaLevel); } }
+        public double NotionalLimitState { get { return Percentage(notional, notionalLevel); } }
+
+        private ObservableCollection<Topic> childTopics = new ObservableCollection<Topic>();
+        public ObservableCollection<Topic> ChildTopics { get { return childTopics; } set { childTopics = value; } }
+
+        public Topic(){}
+
+        public Topic(string name ,string delta , string notional, string outstandbuy, string outstandSell )
+        {
+            this.Name = name;
+            this.Delta = delta;
+            this.Notional = notional;
+            this.OutstandBuy = outstandbuy;
+            this.OutstandSell = outstandSell;
+        }
+
+
+
+        private static double Percentage(double top, double bottom)
+        {
+            return (Math.Abs(top / bottom) * ratio);
+        }
+    }
+}
