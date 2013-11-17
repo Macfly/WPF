9fed86e5fa91a60fd924e1f84867be7548c0f3f1
xavier <xavierbollart@orange.fr>
2013-11-17 23:59:16 +0100

    add data validation

---
+++ WPF/sample/DataValidation/EmailValidationRule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace DataValidation
+{
+    public class EmailValidationRule : ValidationRule
+    {
+        private readonly string smtp = "@gmail.com";
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var str = value as string;
+            if (String.IsNullOrEmpty(str))
+                return new ValidationResult(false, "Please enter a valide email Address.");
+
+            //Match match = Regex.Match(str, @"^([\w.]+)@gmail.com$", RegexOptions.IgnoreCase);
+            Match match = Regex.Match(str, @"^([\w.]+)"+ smtp +"$", RegexOptions.IgnoreCase);
+            if (!match.Success)
+                return new ValidationResult(false, "the adress must be a gmail adress ("+smtp+")");
+
+            return new ValidationResult(true, null);
+        }
+    }
+}
---
+++ WPF/sample/DataValidation/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataValidation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static readonly DependencyProperty MailAddressProperty =
+        DependencyProperty.Register("MailAddress", typeof(string),
+        typeof(MainWindow), new UIPropertyMetadata("xbollart@gmail.com"));
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string MailAddress
+        {
+            get { return (string)GetValue(MailAddressProperty); }
+            set { SetValue(MailAddressProperty, value); }
+        }
+    }
+}
