2961cca7e90c101b304cd8e25f53a5a45b8bca31
xavier <xavierbollart@orange.fr>
2014-01-20 17:57:54 +0100

    Add PriceMonitor project

---
+++ WPF/MVVM DataGrid/Akuna.PriceMonitor/Model/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Akuna.PriceMonitor.Model
+{
+    class Order
+    {
+        const bool bid = true;
+        const bool ask = false;
+        public double Price { get; set; }
+        public enum SideType { bid, ask };
+
+        public SideType Side { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Order(string price,string quantity, bool orderSide )
+        {
+            Price = double.Parse(price);
+            Quantity = int.Parse(quantity);
+
+            if (orderSide == bid)
+                Side = SideType.bid;
+            else
+                Side = SideType.ask;
+        }
+    }
+}
